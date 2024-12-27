ace3c2af566dc1c781e266aa0755d22f9762dad8
Caio Vinicius <caiocvam3@gmail.com>
2024-12-26 22:47:51 -0300

    feat: Implementation of insertUser

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -3,12 +3,24 @@ import UsersService from "../service/UsersService";
 import User from "../models/User";
 
 /*
-  No controller fica a lógica de validação e a conversão do json para o tipo de dados da aplicação
+  No controller fica a lógica de validação e a conversão do json para o 
+  tipo de dados da aplicação
 */
 export default class UsersController {
-  static insertUser(req: Request, res: Response) : void {
-    const user = new User("a", 0);
-    UsersService.insertUser(user);
+  static async insertUser(req: Request, res: Response) : Promise<void> {
+    const {name, age} = req.body
+    try {
+      if(!name || !age){
+        res.status(400).json({ error: "Nome e idade são obrigatórios" });
+      }
+      const user = new User(name, age);
+      UsersService.insertUser(user);
+      res.status(201).json({message: "Usuário criado com sucesso", user});
+    } catch (error) {
+      //console.error(error);
+      res.status(500).json({ error: "Erro ao criar o usuário" });
+      //throw("Erro ao informar valores");
+    }
   }
 
   static updateUser(req: Request, res: Response) : void {
