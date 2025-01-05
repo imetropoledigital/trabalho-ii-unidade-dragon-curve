2f6b53052d866854667cd2ed290096f2d75688ba
Ian Gabriel <ianeigor12@gmail.com>
2025-01-04 22:23:26 -0300

    feat: adding remaining endpoints

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,51 +1,82 @@
 import { Request, Response } from "express";
 import UsersService from "../service/UsersService";
-import User from "../models/User";
+
+const DEFAULT_PAGE = 1;
+const DEFAULT_PER_PAGE = 10;
+const DEFAULT_QUERY = {};
+const DEFAULT_FIELDS = "";
+
+type ListUsersQuery = {
+  page?: number,
+  perPage?: number,
+  fields?: string,
+  query?: string
+};
+
+type GetUserQuery = {
+  fields?: string
+};
+
+type CreateUserBody = {
+  name: string,
+  age: number
+}
+
+type UpdateUserBody = {
+  name?: string,
+  age?: number
+}
+
 
 /*
   No controller fica a lógica de validação e a conversão do json para o 
   tipo de dados da aplicação
 */
 export default class UsersController {
-  static async insertUser(req: Request, res: Response) : Promise<void> {
-    const {name, age} = req.body
+  static validateInsertBody(body: CreateUserBody): void {
+    if(!body.name) throw TypeError('Campo "name" é obrigatório');
+    if(!body.age) throw TypeError('Campo "age" é obrigatório');
+  }
+
+  static async insertUser(req: Request<{}, any, CreateUserBody, {}>, res: Response){
     try {
-      if(!name || !age){
-        res.status(400).json({ error: "Nome e idade são obrigatórios" });
-        return;
+      this.validateInsertBody(req.body);
+      const insertedUser = await UsersService.insertUser(req.body.name, req.body.age);
+      return res.status(201).json({message: "Usuário criado com sucesso", user: insertedUser});
+    } catch (error: unknown) {
+      if(error instanceof TypeError) {
+        return res.status(400).json({message: error.message});
+      } else {
+        return res.status(500).json({message: "erro inesperado no servidor"});
       }
-      const insertedUser = await UsersService.insertUser(name, age);
-      res.status(201).json({message: "Usuário criado com sucesso", user: insertedUser});
-    } catch (error) {
-      //console.error(error);
-      res.status(500).json({ error: "Erro ao criar o usuário" });
-      //throw("Erro ao informar valores");
     }
   }
 
-  static async updateUser(req: Request, res: Response) {
+  static async updateUser(req: Request<{id: string}, any, UpdateUserBody, {}>, res: Response) {
     const {name, age} = req.body
     const { id } = req.params;
-    try {
-      if(!name || !age){
-        res.status(400).json({ error: "Nome e idade são obrigatórios" });
-        return;
-      }
-
-      const updatedUser = await UsersService.updateUser(id, name, age);
-      res.status(201).json({message: "Usuário atualizado com sucesso", user: updatedUser});
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Erro ao atualizar o usuário" });
-      //throw("Erro ao informar valores");
-    }
+    const updatedUser = await UsersService.updateUser(id, name, age);
+    return res.status(201).json({message: "Usuário atualizado com sucesso", user: updatedUser});
   }
 
-  static listUsers(req: Request, res: Response) : void {
-    throw new Error("Method unimplemented");
+  static async listUsers(req: Request<{}, any, {}, ListUsersQuery>, res: Response) {
+    const {page, perPage, query, fields} = req.query;
+    const users = await UsersService.listUsers(
+      page || DEFAULT_PAGE,
+      perPage || DEFAULT_PER_PAGE,
+      query  === undefined ? DEFAULT_QUERY : JSON.parse(query),
+      fields === undefined ? DEFAULT_FIELDS : fields.replace(',', ' ')
+    );
+    return res.status(200).json({page, perPage, result: users});
   }
 
-  static getUser(req: Request, res: Response) : void {
-    throw new Error("Method unimplemented");
+  static async getUser(req: Request<{id: string}, any, {}, GetUserQuery>, res: Response) {
+    const {id} = req.params;
+    const {fields} = req.query;
+    const user = await UsersService.getUser(
+      id, 
+      fields === undefined ? DEFAULT_FIELDS : fields.replace(',', ' ')
+    );
+    return res.status(200).json({user});
   }
 }
--- src/service/UsersService.ts
+++ src/service/UsersService.ts
@@ -1,6 +1,5 @@
 import User from "../models/User";
 
-
 /**
  * No service ficam as regras de negócio. 
  * Para este pequeno projeto também ficam no service as operações no banco de dados.
@@ -11,15 +10,25 @@ export default class UsersService {
     return await user.save();
   }
 
-  static async updateUser(userId: string, newName: string, newAge: number) {
+  static async updateUser(userId: string, newName?: string, newAge?: number) {
    return await User.findByIdAndUpdate(userId, {name: newName, age: newAge}, {new: true}).exec();
   }
 
-  static listUsers() {
-    throw new Error("Method unimplemented");
+  static async listUsers(
+    page: number = 1, 
+    perPage: number = 10, 
+    query: Object = {}, 
+    fields: string = ''
+  ) {
+    const list = await User.find(query, fields, {
+      skip: (page-1)*perPage,
+      limit: perPage
+    }).exec();
+    return list;
   }
 
-  static getUser(userId: number) {
-    throw new Error("Method unimplemented");
+  static async getUser(userId: string, fields: string) {
+    const user = await User.findById(userId, fields);
+    return user;
   }
 }
