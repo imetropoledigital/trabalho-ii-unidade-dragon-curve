a2c6aa556580e8d889cb42971bd92b218762a089
Ian Gabriel <ianeigor12@gmail.com>
2025-01-06 21:20:12 -0300

    fix: express build error with tsc

--- src/router.ts
+++ src/router.ts
@@ -10,9 +10,17 @@ router.get("/hello-world", (request, response) => {
   });
 });
 
-router.post("/users", UsersController.insertUser);
-router.get("/users", UsersController.listUsers);
-router.get("/users/:id", UsersController.getUser);
-router.put("/users/:id", UsersController.updateUser);
+router.post("/users", (req, res) => {
+  UsersController.insertUser(req, res);
+});
+router.get("/users", (req, res) => {
+  UsersController.listUsers(req, res);
+});
+router.get("/users/:id", (req, res) => {
+  UsersController.getUser(req, res);
+});
+router.put("/users/:id", (req, res) => {
+  UsersController.updateUser(req, res)
+});
 
 export default router;
