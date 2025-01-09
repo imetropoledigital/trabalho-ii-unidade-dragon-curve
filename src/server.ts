4b8830b35ee6966001fd09499ceeb85406f75f3a
Caio Vinicius <caiocvam3@gmail.com>
2025-01-09 20:47:29 -0300

    doc: Add server

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,17 @@ const DB_URL = "mongodb://127.0.0.1:27017/test"
 app.use(express.json());
 app.use(myrouter);
 
+/**
+ * Inicializa o servidor da aplicação.
+ * 
+ * Esta função realiza a conexão com o banco de dados MongoDB utilizando 
+ * o Mongoose e, em seguida, inicia o servidor Express na porta 3001.
+ * 
+ * @async
+ * @function
+ * @returns {Promise<void>} Uma promessa que é resolvida após a conexão com o banco de dados e o início do servidor.
+ */
+
 async function initServer() {
   console.log("Connecting to database")
   await mongoose.connect(DB_URL);
