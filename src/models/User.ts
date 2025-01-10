a0140ff1b32305d719b0c5bad7f0c2bcf2a11f7f
Caio Vinicius <caiocvam3@gmail.com>
2025-01-09 21:46:53 -0300

    doc: Add User

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,9 @@
 import mongoose, { Schema } from "mongoose";
 
+/**
+* Criação do schema do banco, onde poderá ser cadastrado o nome e a idade.
+*/
+
 const schema = new mongoose.Schema({
   name: String,
   age: Number
